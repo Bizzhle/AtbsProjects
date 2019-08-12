8b8bd76a5e6ae06c4206c4dd0eb184d7ef6de860
Paul Egbo <dustyripe@gmail.com>
2019-08-12 21:07:02 +0300

    bulletPointAdder

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -1,6 +1,6 @@
 #! python3
-# bulletPointAdder.py - Adds Wikipedia bullet points to the start of 
-# each line of tex on the clipboard.
+# bulletPointAdder - Adds wiki bullet points to the start of each line
+# of text on the clipboard
 
 
 import pyperclip 
